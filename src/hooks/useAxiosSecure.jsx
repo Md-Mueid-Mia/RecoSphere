07de6cf49d331f7f94bc2bc29320efab6dd84bda
Md Mueid MiaMd-Mueid-Mia <mdmueidmia@gmail.com>
2024-12-26 10:28:19 +0600

    checking this website

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,10 +2,11 @@ import axios from "axios";
 import { useContext, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import AuthContext from "../provider/AuthContext";
+import toast from "react-hot-toast";
 
 export const axiosSecure = axios.create({
-  baseURL: 'https://assignment-11-server-gules-three.vercel.app',
-// baseURL: 'http://localhost:5000',
+//   baseURL: 'https://assignment-11-server-gules-three.vercel.app',
+baseURL: 'http://localhost:5000',
   withCredentials: true,
 });
 
@@ -25,6 +26,8 @@ const useAxiosSecure=()=>{
                     .then(res=>{
                         console.log('logout', res)
                         // navigate to login page
+                        // be careful you are not authorized to access this page
+                        toast.error('You are not authorized to access this page. Please login first.')
                     navigate('/login')
                     })
                     .catch(err=>{
